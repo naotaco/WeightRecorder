5e9c1cda4ee3f62536852373eecea3272303b171
Nao Taco <naotaco@gmail.com>
2017-09-03 03:35:09 +0900

    Save each trial data to separated files

--- WeightRecorder/MainWindow.xaml.cs
+++ WeightRecorder/MainWindow.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.IO.Ports;
 
 namespace WeightRecorder
 {
@@ -23,6 +24,85 @@ namespace WeightRecorder
         public MainWindow()
         {
             InitializeComponent();
+            Connect();
+        }
+
+        SerialPort serial = null;
+        WeightHistory history = null;
+
+        private void ConnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            Connect();
+        }
+
+        private void Connect()
+        {
+            if (serial != null && serial.IsOpen)
+            {
+                Console.WriteLine("It's already opened. Close first.");
+                return;
+            }
+            serial = new SerialPort(ComPort.Text, 9600, Parity.None, 8, StopBits.One);
+            serial.NewLine = Environment.NewLine;
+            serial.DataReceived += Serial_DataReceived;
+            serial.ErrorReceived += Serial_ErrorReceived;
+            serial.DtrEnable = true;
+            try
+            {
+                serial.Open();
+            }
+            catch (System.IO.IOException ex)
+            {
+                Console.WriteLine("Failed to connect: " + ex.Message);
+                return;
+            }
+
+            Console.WriteLine("Connected");
+        }
+
+        private void Serial_ErrorReceived(object sender, SerialErrorReceivedEventArgs e)
+        {
+            Console.WriteLine("error received.");
+        }
+
+        private void Serial_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            if (history == null)
+            {
+                history = new WeightHistory();
+            }
+
+            var raw = serial?.ReadExisting();
+            var lines = raw.Split(new string[] { Environment.NewLine }, StringSplitOptions.RemoveEmptyEntries);
+            foreach (var line in lines)
+            {
+                var data = line.Split(new char[] { ' ' });
+                if (data.Length == 5)
+                {
+                    history.Add(
+                        int.Parse(data[0], System.Globalization.NumberStyles.HexNumber),
+                        int.Parse(data[2]));
+                }
+                else
+                {
+                    Console.WriteLine("Invalid length of data. " + data);
+                }
+
+            }
+        }
+
+        private void DisconnectButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Disconnect.");
+            serial?.Close();
+            serial?.Dispose();
+            serial = null;
+        }
+
+        private void DumpButton_Click(object sender, RoutedEventArgs e)
+        {
+            Console.WriteLine("Dump");
+            history?.Dump();
         }
     }
 }
---
+++ WeightRecorder/WeightHistory.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeightRecorder
+{
+    public class WeightHistory
+    {
+        List<WeightRecord> History = new List<WeightRecord>();
+        public WeightHistory() { }
+        const int WEIGHT_THRESHOLD = 100; // ignore less than 100g
+        const int NO_LOAD_TIME = 100; // 100 samples
+
+        public void Dump()
+        {
+            var temp_history = new List<WeightRecord>(History);
+
+            var maxWeight = 0;
+            foreach (var r in temp_history)
+            {
+                maxWeight = Math.Max(maxWeight, r.Weight);
+            }
+
+            var filename = DateTime.Now.ToString().Replace('/', '-').Replace(':', '-').Replace(' ', '-') + "_Max" + maxWeight + "g.csv";
+            int lastTime = 0;
+            int upper2Byte = 0;
+
+            using (StreamWriter writer = new StreamWriter(filename, false))
+            {
+                StringBuilder sb = new StringBuilder();
+                foreach (var r in temp_history)
+                {
+                    if (lastTime - r.TimeInMillis > (60000 * (upper2Byte + 1)))
+                    {
+                        // maybe clock reset
+                        upper2Byte++;
+                    }
+
+                    r.TimeInMillis += upper2Byte * 65536;
+                    sb.AppendFormat("{0},{1}{2}", r.TimeInMillis, r.Weight, Environment.NewLine);
+                    //Console.Write("time: {0} weight {1}\n", r.TimeInMillis, r.Weight);
+                    lastTime = r.TimeInMillis;
+                }
+                writer.Write(sb.ToString());
+            }
+            Console.WriteLine(temp_history.Count + " records saved (~" + lastTime + " ms) : " + filename);
+            History.Clear();
+        }
+
+        bool recording = false;
+
+        public void Add(int time, int weight)
+        {
+            History.Add(new WeightRecord() { TimeInMillis = time, Weight = weight });
+
+
+            if (!recording)
+            {
+                if (NoLoad(History, 0, History.Count))
+                {
+                    History.RemoveAt(0);
+                }
+                else if (History.Count > NO_LOAD_TIME)
+                {
+                    recording = true;
+                }
+            }
+            else
+            {
+                // recording
+                if (NoLoad(History, History.Count - NO_LOAD_TIME, History.Count))
+                {
+                    Dump();
+                    recording = false;
+                }
+            }
+
+        }
+
+        bool NoLoad(List<WeightRecord> record, int start, int last)
+        {
+            if (record.Count < NO_LOAD_TIME) { return false; }
+
+            var max = record[0].Weight;
+            for (int i = start; i < last; i++)
+            {
+                max = Math.Max(record[i].Weight, max);
+            }
+
+            return max < WEIGHT_THRESHOLD;
+        }
+    }
+
+    public class WeightRecord
+    {
+        public int TimeInMillis { get; set; } = 0;
+        public int Weight { get; set; } = 0;
+    }
+}
